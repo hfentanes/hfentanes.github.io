45be3454581569b442e0971743012eaaeca5bc11
Hadassa Fentanes <hadassafentanes@gmail.com>
2023-11-15 02:48:34 +0100

    Otros proyectos se acortan cuando uno se expande

--- scripts/shortDescription.js
+++ scripts/shortDescription.js
@@ -5,19 +5,19 @@ const short = function (text, max) {
   return newText;
 };
 
-const switchLength = function (element) {
+const switchLength = function (element, close = false) {
     const active = element.getAttribute("data-active");
     const original = element.getAttribute("data-original");
     const elButton = element.querySelector("button");
     const elParagraph = element.querySelector("p");
     const longDescription = elParagraph.innerHTML;
-    const shortDescription = short(elParagraph.innerHTML, 140);
+    const shortDescription = short(original || elParagraph.innerHTML, 140);
 
     if (!original) {
         element.setAttribute("data-original", longDescription);
     }
 
-    if (active === "true"){
+    if (active === "true" || close){
         elParagraph.innerHTML = shortDescription;
         element.setAttribute("data-active", "false");
         elButton.innerHTML = "Ver más";
@@ -28,18 +28,23 @@ const switchLength = function (element) {
     }
 }
 
-const addShortifySystem = function (element) {
+const addShortifySystem = function (element, elShortify) {
     const elButton = element.querySelector("button");
 
     elButton.addEventListener("click", function(){
+        elShortify.forEach(shortify => {
+            switchLength(shortify, true);
+            console.log(shortify);
+        })
         switchLength(element);
     });
 }
 
 document.addEventListener("DOMContentLoaded", function (){
     const elShortify = document.querySelectorAll(".shortify");
+
     elShortify.forEach(shortify => {
         switchLength(shortify);
-        addShortifySystem(shortify);
+        addShortifySystem(shortify, elShortify);
     })
 });
