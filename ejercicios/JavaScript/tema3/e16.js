62d1f1d27f1b1188189c344a823ab1a6b00a396a
Hadassa Fentanes <hadassafentanes@gmail.com>
2023-11-24 23:35:33 +0100

    Creación de la rama css/e07

--- ejercicios/JavaScript/tema3/e16.js
+++
@@ -1,9 +0,0 @@
-//datos
-let isSunnyDay = true;
-let isHomeworkCompleted = true;
-
-//ternario
-let text = isSunnyDay && isHomeworkCompleted ? "Jesse can go out to play." : "Jesse stays at home.";
-
-//sale en la pantalla
-console.log(text);
