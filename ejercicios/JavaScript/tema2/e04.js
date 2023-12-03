7cff967017e96a07717e690dea3c7122ed489c42
Hadassa Fentanes <hadassafentanes@gmail.com>
2023-12-03 20:16:33 +0100

    Creación de la rama css/e12

--- ejercicios/JavaScript/tema2/e04.js
+++
@@ -1,9 +0,0 @@
-//declaro las variables (con CONST porque un nombre, en general, no se cambia)
-const firstName = "Hadassa";
-const lastName = "Fentanes";
-
-//uno las dos variables en una
-const fullName = firstName + " " + lastName;
-
-//lo que sale en pantalla
-console.log("Mi nombre es " + fullName + ".")
